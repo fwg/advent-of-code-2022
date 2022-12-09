cef58a65b2ed83d964bfd50834fb8560d5d36645
Friedemann Altrock <hallo@faltrock.de>
2022-12-09 18:12:04 +0100

    day 6 part 2 in rust .... yay first contact with lifetimes in error messages

--- rust/src/bin/day06.rs
+++ rust/src/bin/day06.rs
@@ -5,14 +5,25 @@ fn main() {
     let (at, _marker) = input
         .windows(4)
         .enumerate()
-        .find(|(_n, window)| {
-            !(window[0] == window[1]
-                || window[0] == window[2]
-                || window[0] == window[3]
-                || window[1] == window[2]
-                || window[1] == window[3]
-                || window[2] == window[3])
-        })
+        .find(unique_marker)
         .expect("No four-char marker found");
     println!("part 1: {}", 4 + at);
+
+    let (at, _marker) = input
+        .windows(14)
+        .enumerate()
+        .find(unique_marker)
+        .expect("No fourteen-char marker found");
+    println!("part 1: {}", 14 + at);
+}
+
+fn unique_marker((_n, window): &(usize, &[char])) -> bool {
+    for i in 0..window.len() - 1 {
+        for j in i + 1..window.len() {
+            if window[i] == window[j] {
+                return false;
+            }
+        }
+    }
+    true
 }
