429ea51920740c65b2a7fb0007d1069d13c32067
Friedemann Altrock <hallo@faltrock.de>
2022-12-12 13:18:40 +0100

    day 12 part 2 php - naive approach, takes too long

--- php/day12.php
+++ php/day12.php
@@ -38,50 +38,74 @@ if (!($end_point && $start_point)) {
     die("S and E must be defined!");
 }
 
-$positions = [$end_point];
-$visited = [$end_point . '' => true];
-$steps = 0;
-$directions = [
-    ['x' => -1, 'y' => 0],
-    ['x' => 1, 'y' => 0],
-    ['x' => 0, 'y' => -1],
-    ['x' => 0, 'y' => 1],
-];
+function path_length(array $map, Point $start_point, Point $end_point): int
+{
+    $positions = [$end_point];
+    $visited = [$end_point . '' => true];
+    $steps = 0;
+    $directions = [
+        ['x' => -1, 'y' => 0],
+        ['x' => 1, 'y' => 0],
+        ['x' => 0, 'y' => -1],
+        ['x' => 0, 'y' => 1],
+    ];
 
-// go back from end point: move to each viable position
-while (!isset($visited[$start_point . ''])) {
-    $new_positions = [];
-    foreach ($positions as $point) {
-        $h0 = $map[$point->y][$point->x];
-        // look into all directions
-        foreach ($directions as $direction) {
-            $y = $point->y + $direction['y'];
-            $x = $point->x + $direction['x'];
-            if (!isset($map[$y][$x])) {
-                continue;
+    // go back from end point: move to each viable position
+    while (!isset($visited[$start_point . ''])) {
+        $new_positions = [];
+        foreach ($positions as $point) {
+            $h0 = $map[$point->y][$point->x];
+            // look into all directions
+            foreach ($directions as $direction) {
+                $y = $point->y + $direction['y'];
+                $x = $point->x + $direction['x'];
+                if (!isset($map[$y][$x])) {
+                    continue;
+                }
+                $h1 = $map[$y][$x];
+                // note: this is the reverse from the description on AoC
+                // can go arbitrarily far up
+                // can go equal height
+                // can go 1 down
+                if (!($h1 >= $h0 - 1)) {
+                    // we can't go here
+                    continue;
+                }
+                $p = new Point($x, $y);
+                // we've already visited this
+                if (isset($visited[$p . ''])) {
+                    continue;
+                }
+                $new_positions[] = $p;
+                $visited[$p . ''] = true;
             }
-            $h1 = $map[$y][$x];
-            // note: this is the reverse from the description on AoC
-            // can go arbitrarily far up
-            // can go equal height
-            // can go 1 down
-            if (!($h1 >= $h0 - 1)) {
-                // we can't go here
-                continue;
-            }
-            $p = new Point($x, $y);
-            // we've already visited this
-            if (isset($visited[$p . ''])) {
-                continue;
-            }
-            $new_positions[] = $p;
-            $visited[$p . ''] = true;
         }
+
+        // we've taken 1 step in possibly multiple directions
+        $steps += 1;
+        $positions = $new_positions;
     }
 
-    // we've taken 1 step in possibly multiple directions
-    $steps += 1;
-    $positions = $new_positions;
+    return $steps;
 }
 
-echo "part 1: ", $steps, PHP_EOL;
+// part 1: find path length from S to E
+echo "part 1: ", path_length($map, $start_point, $end_point), PHP_EOL;
+
+// part 2: find shortest path from all points in the map at h=0
+$min = PHP_INT_MAX;
+foreach ($map as $y => $row) {
+    foreach ($row as $x => $h) {
+        if ($h > 0) {
+            continue;
+        }
+        $p = new Point($x, $y);
+        $l = path_length($map, $p, $end_point);
+        echo "part 2 starting from {$p} reaching E in {$l} steps\n";
+        if ($l < $min) {
+            $min = $l;
+        }
+    }
+}
+
+echo "part 2: ", $min, PHP_EOL;
